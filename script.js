e50b2a7161a0ee18eb6441402dffe84f9cdb5dec
Mike Hotchkiss <sxs.linux@hotmail.com>
2020-04-27 14:18:15 -0400

    Reworded some comments to make them easier to read

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 let totalSum = 0;
 let userInput = 0;
 
-// Create a loop to create the 0-9 key
+// Creating a loop to create the 0-9 key
 const numberButtonArray = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '/', '*', '=' ];
 for (let i = 0; i != numberButtonArray.length; i++) {
 	document.querySelector(`#calculator`).innerHTML += `
     <button id="calc-num-${numberButtonArray[i]}">${numberButtonArray[i]}</button>`;
 }
 
-// Creating minus key seperate since it was breaking a loop
+// Creating the minus key seperate since it was breaking a loop
 function minusKey() {
 	document.querySelector(`#calculator`).innerHTML += `<button id="minus-key">-</button>`;
 }
 
-// Calling minusKey function
+// Calling the minusKey function to get key displayed on the page
 minusKey();
 
-// Creating one function for key inputs
+// Creating a function to update the display with the user key inputs
 function keyPress(inputKey) {
 	document.querySelector(`#calc-output`).value += inputKey;
 }
 
-// Creating a function to do the math PEMDAS
+// Creating a function to do the math
 function doMath() {
 	totalSum = eval(userInput);
 }
@@ -40,7 +40,7 @@ function clearButton() {
 // Adding event listener for the clear key
 document.querySelector(`#calc-num-clear`).addEventListener('click', clearButton);
 
-// Creating the listener
+// Creating the listener using a loop to get 
 document.querySelector(`#calculator`).addEventListener('click', (buttonPress) => {
 	for (i = 0; i != numberButtonArray.length; i++) {
 		if (event.target.id.includes('=')) {
